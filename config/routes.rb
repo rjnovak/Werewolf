e3a82eb799a26cb2de5db78e30d4c5193c510fdd
Ryan Novak <rjnovak@email.wm.edu>
2013-10-28 14:35:51 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Werewolf::Application.routes.draw do
   resources :users
   resources :players
 
-  devise_for :users
-
   root :to => 'players#index'
   match '/kill/:killerid/:victimid', to: 'kill#show', :constraints => {:killerid => /[0-9]*/, :victimid => /[0-9]*/}
   match '/loc/:playerid/:nlat/:nlon', to: 'loc#setloc', :constraints => {:playerid => /[0-9]*/, :nlat => /[0-9]*/, :nlon => /[0-9]*/}
