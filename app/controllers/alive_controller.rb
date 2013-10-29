bab3f8fc70c5b55f45b773950cd2c9f6998cade3
Ryan Novak <rjnovak@email.wm.edu>
2013-10-28 21:53:44 -0400

    Modifying alive

--- app/controllers/alive_controller.rb
+++ app/controllers/alive_controller.rb
@@ -3,7 +3,7 @@ class AliveController < ApplicationController
   	@players = Player.where('is_dead = "f"')
 
     respond_to do |format|
-      #format.html # index.html.erb
+      format.html # index.html.erb
       format.json { render json: @players }
     end
   end
