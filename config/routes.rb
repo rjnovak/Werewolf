13c17488557ed7228c68df79c44d15cfad427d16
Ryan Novak <rjnovak@email.wm.edu>
2013-10-31 14:30:17 -0400

    sigh

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Werewolf::Application.routes.draw do
   namespace :api do
     resources :players
     resources :games
+    devise_scope :user do
+      post 'registrations' => 'registrations#create', :as => 'register'
+      post 'sessions' => 'sessions#create', :as => 'login'
+      delete 'sessions' => 'sessions#destroy', :as => 'logout'
+    end
   end
 
   devise_for :users
